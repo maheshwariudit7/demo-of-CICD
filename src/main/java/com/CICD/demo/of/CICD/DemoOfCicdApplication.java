11ca0265bd8161f31259ce64f2cca80057cef36e
maheshwariudit7 <maheshwariudit7@gmail.com>
2024-12-24 19:21:54 +0530

    first commit

---
+++ src/main/java/com/CICD/demo/of/CICD/DemoOfCicdApplication.java
@@ -0,0 +1,13 @@
+package com.CICD.demo.of.CICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOfCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOfCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CICD/demo/of/CICD/DemoOfCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CICD.demo.of.CICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOfCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
